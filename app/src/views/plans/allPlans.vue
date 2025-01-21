f5fae38285717fcfa34b7dd98a60a04b7f32151d
miguelamoreira <40210256@esmad.ipp.pt>
2025-01-21 13:53:47 +0000

    altered website name & favicon

--- app/src/views/plans/allPlans.vue
+++ app/src/views/plans/allPlans.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
+import PlanCards from '@/components/dashboard/PlanCards.vue';
 </script>
 <template>
     <v-row>
         <v-col cols="12" md="12">
             <UiParentCard title="All Plans"> 
-                <div class=""><p class="text-body-1">Plans</p></div>
+                <PlanCards></PlanCards>
             </UiParentCard>
         </v-col>
     </v-row>
